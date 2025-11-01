999a662ed751996717f9a6bd8d153dda161628c7
taise-hikawa <cmmkarubi@gmail.com>
2025-11-01 15:33:31 +0900

    Add destroy action with tests

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -34,6 +34,12 @@ class GoalsController < ApplicationController
     end
   end
 
+  def destroy
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+    redirect_to goals_path
+  end
+
   def goal_params
     params.require(:goal).permit(:title, :description, :deadline)
   end
